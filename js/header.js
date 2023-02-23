42cf13b3ce97b82eae7350b7ee03d40ced03e2a1
escv12 <62186356+escv12@users.noreply.github.com>
2023-02-23 17:23:57 +0900

    index 전체 수정 및 로고 변경
    index 전체 수정 및 로고 변경

--- js/header.js
+++ js/header.js
@@ -1,14 +1,38 @@
 window.onload = function () {
+
+
+    /* 스크롤시 헤더 숨김 / 보임 처리 */
     const header = document.getElementById("header_wrap");
     let prevScrollpos = window.pageYOffset;
 
-    window.addEventListener('scroll', function () {
+    window.addEventListener('wheel', function (event) {
         const currentScrollPos = window.pageYOffset;
-        if (prevScrollpos > currentScrollPos) {
-            header.classList.remove('header--hidden');
-        } else {
+        if (event.deltaY > 0) {
+            // User scrolled down with the wheel
             header.classList.add('header--hidden');
+        } else {
+            // User scrolled up with the wheel
+            header.classList.remove('header--hidden');
         }
         prevScrollpos = currentScrollPos;
+    });
+
+
+    /* 드롭 메뉴 몇번째 hover */
+    const listItems = document.querySelectorAll('.header_nav .header_nav_item');
+
+
+    listItems.forEach((li, index) => {
+        li.addEventListener('mouseover', () => {
+            const dropDownElement = li.querySelector('.drop_down');
+            if(!dropDownElement) return;
+            dropDownElement.style.display = 'flex';
+        });
+
+        li.addEventListener('mouseout', () => {
+            const dropDownElement = li.querySelector('.drop_down');
+            if(!dropDownElement) return;
+            dropDownElement.style.display = 'none';
+        });
     });
 }
